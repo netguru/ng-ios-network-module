a37ea6dd17b48756dffd8f3626cf8ef9b688e8f7
Esat Kemal Ekren <esat.kemal.ekren@netguru.com>
2023-01-25 15:26:03 +0100

    review mocks added

---
+++ NetworkModuleSampleApp/NetworkModuleSampleApp/PreviewMocks/PreviewMocks.swift
@@ -0,0 +1,18 @@
+//
+//  PreviewMocks.swift
+//  NetworkModuleSampleApp
+//
+//  Created by Kemal Ekren on 25/01/2023.
+//
+
+import Foundation
+
+enum PreviewMocks {
+    static let mockEpisodeModel = EpisodeModel(id: "",
+                                        name: "Chapter 1",
+                                        airDate: "01/12/2023",
+                                        director: "John Doe",
+                                        writer: "Jane Doe",
+                                        characters: [],
+                                        imageURL: "")
+}
