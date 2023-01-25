62b93ef8729c73dc443010c00991721ea333209e
Esat Kemal Ekren <esat.kemal.ekren@netguru.com>
2023-01-25 15:32:59 +0100

    Chracter scene fix

--- NetworkModuleSampleApp/NetworkModuleSampleApp/Scenes/Character/CharacterScene.swift
+++ NetworkModuleSampleApp/NetworkModuleSampleApp/Scenes/Character/CharacterScene.swift
@@ -8,7 +8,7 @@ import SwiftUI
 struct CharacterScene: View {
     var body: some View {
         //TODO: Will be created
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Charater Scene")
     }
 }
 
