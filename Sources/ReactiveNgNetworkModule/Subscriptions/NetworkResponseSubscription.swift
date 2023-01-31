518a383b10e8a43a8790baf1950bf5ff8bface81
Pawel Kozielecki <pawel.kozielecki@gmail.com>
2023-01-31 11:04:53 +0100

    BN-4301 [FIX] Start network request when the subscription is received

--- Sources/ReactiveNgNetworkModule/Subscriptions/NetworkResponseSubscription.swift
+++ Sources/ReactiveNgNetworkModule/Subscriptions/NetworkResponseSubscription.swift
@@ -13,7 +13,10 @@ public extension Publishers {
     final class NetworkResponseSubscription<S: Subscriber>: Subscription
         where S.Input == NetworkResponse, S.Failure == NetworkError {
 
+        private let networkModule: NetworkModule
         private var subscriber: S?
+        private var request: NetworkRequest?
+        private var urlRequest: URLRequest?
 
         /// A default NetworkResponseSubscription initializer.
         /// To be used when executing a generic URLRequest.
@@ -24,15 +27,9 @@ public extension Publishers {
         ///   - subscriber: a subscriber to be notified.
         public init(urlRequest: URLRequest, networkModule: NetworkModule, subscriber: S) {
             self.subscriber = subscriber
-            networkModule.perform(urlRequest: urlRequest) { result in
-                switch result {
-                case let .success(response):
-                    _ = subscriber.receive(response)
-                    subscriber.receive(completion: Subscribers.Completion.finished)
-                case let .failure(error):
-                    subscriber.receive(completion: Subscribers.Completion.failure(error))
-                }
-            }
+            self.networkModule = networkModule
+            self.urlRequest = urlRequest
+            request = nil
         }
 
         /// A supplementary NetworkResponseSubscription initializer.
@@ -44,23 +41,50 @@ public extension Publishers {
         ///   - subscriber: a subscriber to be notified.
         public init(request: NetworkRequest, networkModule: NetworkModule, subscriber: S) {
             self.subscriber = subscriber
-            networkModule.perform(request: request) { result in
-                switch result {
-                case let .success(response):
-                    _ = subscriber.receive(response)
-                    subscriber.receive(completion: Subscribers.Completion.finished)
-                case let .failure(error):
-                    subscriber.receive(completion: Subscribers.Completion.failure(error))
-                }
-            }
+            self.networkModule = networkModule
+            self.request = request
+            urlRequest = nil
         }
 
         /// - SeeAlso: Subscription.request(demand:)
-        public func request(_ demand: Subscribers.Demand) {}
+        public func request(_ demand: Subscribers.Demand) {
+            if let request = request {
+                perform(request: request)
+            } else if let urlRequest = urlRequest {
+                perform(urlRequest: urlRequest)
+            }
+        }
 
         /// - SeeAlso: Subscription.cancel()
         public func cancel() {
             subscriber = nil
+        }
+    }
+}
+
+private extension Publishers.NetworkResponseSubscription {
+
+    func perform(request: NetworkRequest) {
+        networkModule.perform(request: request) { [subscriber] result in
+            switch result {
+            case let .success(response):
+                _ = subscriber?.receive(response)
+                subscriber?.receive(completion: Subscribers.Completion.finished)
+            case let .failure(error):
+                subscriber?.receive(completion: Subscribers.Completion.failure(error))
+            }
+        }
+    }
+
+    func perform(urlRequest: URLRequest) {
+        networkModule.perform(urlRequest: urlRequest) { [subscriber] result in
+            switch result {
+            case let .success(response):
+                _ = subscriber?.receive(response)
+                subscriber?.receive(completion: Subscribers.Completion.finished)
+            case let .failure(error):
+                subscriber?.receive(completion: Subscribers.Completion.failure(error))
+            }
         }
     }
 }
